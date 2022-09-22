33c31fd59ce647ab28ca4f99cda2d6e08642a8e5
Anitha Nagadasari <anitha.nagadasarik95@gmail.com>
2022-09-22 16:06:58 +0530

    Final Commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Project Name : **Random Password Generator** 
+![badge](https://img.shields.io/badge/iNeuron-LCO-green) ![badge](https://img.shields.io/badge/Hitesh--Choudhary-Full%20Stack%20Javascript%20Course-orange)
+
+This project developed with pure HTML and CSS mainly focused on Pure Javascript. <br/>
+
+On click of button Random password displayed  in input field and generated password and we can copy the password from input element. 
+If the input value is less then 5 password not generated, instead error message displayed in input element.
+
+<br/>
+
+# Concepts used 
+- Object
+- Math.floor()
+- Math.random()
+- Arrays
+- Arrow functions
+- navigator.clipboard.writeText(value); //  To copy value to clipboard
+
+
+<br/>
+
+# 👩‍💻 Tech Stack Used :
+
+![Javascript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black) ![HTML](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white) ![CSS](https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white) 
+
+<br/>
+
+# Time taken to finish this project :
+
+45 min to finish this project.
+
+### You can Check it Live on Below Link :
+
+[Live Link !](https://pure-js-age-calculator.netlify.app/)
+
+### Demo :
+
+![App Image](https://github.com/anitha-nagadasarink/age-calculator-using-pure-JS/blob/2fa3d5c6fe9feb081092f2bc91466a05ee5dc468/image/demo.PNG)
+
---
+++ script.js
@@ -0,0 +1,80 @@
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+const alphabets = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "~!@#$%^&*()_+-=|`';:<>?/,.{}[]";
+
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
+
+clipboardEl.addEventListener('click', () => {
+    let newPasswordResult = document.getElementById('result').innerText;
+    navigator.clipboard.writeText(newPasswordResult);
+    alert(`Copied the text: ${newPasswordResult}`)
+});
+
+generateEl.addEventListener('click', () => {
+    generatePassword(randomFunc.lower, randomFunc.number, randomFunc.upper, randomFunc.symbol, lengthEl.value);
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+    let passwordResult = [];
+    if (length >= 5) {
+        for (let i = 0; i < length; i++) {
+            passwordResult.push(lower(), upper(), number(), symbol());
+        }
+        let processedPassword = String(passwordResult).replaceAll(",", "").slice(0, length);
+        resultEl.innerText = processedPassword;
+
+    } else if (length < 5) {
+        resultEl.innerText = "Password length min 5";
+    }
+
+}
+
+function getRandomLower() {
+    if (lowercaseEl.checked) {
+        const randomLowChar = alphabets[Math.floor(Math.random() * 26) + 1];
+        return randomLowChar;
+    }
+
+}
+
+
+function getRandomUpper() {
+    if (uppercaseEl.checked) {
+        const upperChar = alphabets.toUpperCase();
+        const randomUpperChar = upperChar[Math.floor(Math.random() * 26) + 1];
+        return randomUpperChar;
+    }
+
+}
+
+
+function getRandomNumber() {
+    if (numbersEl.checked) {
+        const randomNumbers = numbers[Math.floor(Math.random() * 9) + 1];
+        return randomNumbers;
+    }
+
+}
+
+
+function getRandomSymbol() {
+    if (symbolsEl.checked) {
+        const randomSymbols = symbols[Math.floor(Math.random() * 30) + 1];
+        return randomSymbols;
+    }
+}
